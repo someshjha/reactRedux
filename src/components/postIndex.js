c3bb05693da8b1fb27e52af4134abcdbfb15c421
Somesh Jha <someshj@ca.ibm.com>
2018-08-19 22:20:57 -0400

    init

---
+++ src/components/postIndex.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
+
+class PostsIndex extends Component{
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts(){
+        return _.map(this.props.posts, post => {
+            console.log('POST', post[271830]);
+            return (
+              <li className="list-group-item"> 
+                {post.title}
+              </li>
+            );
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <h3>Posts</h3>
+                <ul className = "list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {posts: state};
+}
+
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
